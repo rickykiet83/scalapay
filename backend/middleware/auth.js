c15315eb81895a761b53aa6c75a59384bb5b779b
Kiet Pham <kietpham.dev@gmail.com>
2021-02-17 10:21:01 +1100

    set development environment and scala_token for dev

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,12 +3,11 @@ const config = require('config');
 module.exports = function (req, res, next) {
     try {
         let token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            token = config.get('token');
-        }
-        // return res.status(401).send('Access denied. No token provided');
+        if (!token) token = config.get('token');
+
+        if (!token) return res.status(401).send('Access denied. No token provided');
+        
         req.token = token;
-    
         next();
     } catch (error) {
         res.status(401).send({ message: "Auth failed!" });
